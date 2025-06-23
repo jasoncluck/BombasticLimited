eb43e7c86e83ecf8480116d5b7a597957a5afd0e
Jason Cluck <jasoncluck@gmail.com>
2025-06-22 19:53:24 -0700

    Fix gap in content select

--- src/lib/components/content/content-select.svelte
+++ src/lib/components/content/content-select.svelte
@@ -23,7 +23,7 @@
   const contentState = getContentState();
 </script>
 
-<div class="flex gap-4 h-[20px] items-center">
+<div class="flex gap-2 h-[20px] items-center">
   <Label for="isSelectionMode" class="text-xs cursor-pointer">Select</Label>
   <div class="w-[20px]">
     <Checkbox
@@ -38,6 +38,8 @@
     />
   </div>
   {#if contentState.isSelectionMode && contentState.selectedVideos.length > 0}
-    <ContentDropdown {playlist} {playlists} {supabase} {session} />
+    <div class="ml-2">
+      <ContentDropdown {playlist} {playlists} {supabase} {session} />
+    </div>
   {/if}
 </div>
