07a6bcd3c52a06144078b082dd519318e685e395
Jason Cluck <jasoncluck@gmail.com>
2025-08-19 10:37:38 -0700

    start of markdown updates

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -5,13 +6,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
-
+  preprocess: [vitePreprocess(), mdsvex()],
   kit: {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter({
+      extensions: ['.svelte', '.svelte.ts', '.svx'],
       csp: {
         directives: {
           'script-src': [
@@ -28,7 +29,6 @@ const config = {
             'https://www.googletagmanager.com',
             'https://googletagmanager.com',
             'https://gql.twitch.tv',
-
             'https://s.ytimg.com',
             'https://embed.twitch.tv/',
             'https://player.twitch.tv/',
@@ -40,7 +40,6 @@ const config = {
           'frame-src': [
             'self',
             'https://www.youtube.com',
-
             'https://www.google.com',
             'https://googleads.g.doubleclick.net',
             'https://googlesyndication.com',
@@ -121,6 +120,7 @@ const config = {
       },
     }),
   },
+  extensions: ['.svelte', '.svx'],
 };
 
 export default config;
