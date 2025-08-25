a76e2f2517b7a0aac9f13ec78bcdd11376d6e8e1
Jason Cluck <jasoncluck@gmail.com>
2025-08-24 19:35:50 -0700

    trying to remove Play button border on mobile

--- src/lib/components/content/shared-content-header.svelte
+++ src/lib/components/content/shared-content-header.svelte
@@ -108,9 +108,9 @@
           variant="ghost"
           size="icon"
           disabled={!nextVideoToPlay}
-          class="bg-primary hover:!bg-primary mr-2 rounded-full p-7 shadow-xl 
-        transition-transform duration-200 hover:scale-105
-        hover:shadow-2xl hover:brightness-[150%]"
+          class="bg-primary hover:!bg-primary mr-2 rounded-full border-none p-7 
+        shadow-xl transition-transform duration-200
+        outline-none hover:scale-105 hover:shadow-2xl hover:brightness-[150%] focus:border-none focus:outline-none active:border-none active:outline-none"
           onclick={handlePlayVideo}
         >
           <Play
