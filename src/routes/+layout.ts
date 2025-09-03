7afd367fe52bdbfec99cc185210a1b0ca164372f
Jason Cluck <jasoncluck@gmail.com>
2025-09-03 15:14:29 -0700

    update

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -6,7 +6,7 @@ export const load = async () => {
     posthog.init('phc_gaW7HPIk0koi0IJ4iILkFIHZgFVz7wJ7Z0tMuw7KW7g', {
       api_host: 'https://alpine.bombastic.ltd',
       ui_host: 'https://us.posthog.com',
-      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+      cookieless_mode: 'always',
 
       // Enable error tracking features
       capture_pageview: true,
