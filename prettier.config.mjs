31193dbf5283af588916357ccb0407a7b55dd06a
Jason Cluck <jasoncluck@gmail.com>
2025-09-10 11:46:39 -0700

    add docs on card/table view

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -20,6 +20,7 @@ const config = {
         parser: 'svelte',
       },
     },
+    { files: '*.svx', options: { parser: 'markdown' } },
     {
       files: '*.sql',
       excludeFiles: ['supabase/seed.sql'],
