ba0e939bf2207b437a84eb053007c4dc5dfb8e7a
Jason Cluck <jasoncluck@gmail.com>
2025-09-11 15:40:14 -0700

    fix video embed

--- src/lib/components/video/video-embed.svelte
+++ src/lib/components/video/video-embed.svelte
@@ -2,4 +2,8 @@
   const { divId }: { divId: string } = $props();
 </script>
 
-<div id={divId} data-testid={`${divId}-test`} class=""></div>
+<div
+  id={divId}
+  data-testid={`${divId}-test`}
+  class="absolute top-0 left-0 h-full w-full"
+></div>
